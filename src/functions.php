48e667ad87c6d4da46e239c1ab43f2ee0df2e106
Heather B <heatherbrokmeier@gmail.com>
2016-08-23 00:10:23 -0500

    Make colors/fonts/quote saveable.

--- src/functions.php
+++ src/functions.php
@@ -10,16 +10,62 @@
 
 
         public function __construct() {
-            $this->quote = $this->random_quote();
-            $this->set_font();
-            $this->set_css();
+            // $this->set_font();
+            if ( isset( $_GET['quote'] ) && isset( $_GET['author'] ) ) {
+                // echo 'QUOTE: ' . $_GET['quote'] . '<br>';
+                // echo 'AUTHOR: ' . $_GET['author'] . '<br>';
+                $this->quote = $this->random_quote( $_GET['quote'], $_GET['author']);
+            } else {
+                $this->quote = $this->random_quote();
+            }
+
+            if ( isset( $_GET['font'] ) ) {
+                // echo 'FONT: ' . $_GET['font'] . '<br>';
+                $this->set_font( $_GET['font'] );
+            } else {
+                $this->set_font();
+            }
+
+            if ( isset( $_GET['bg'] ) && isset( $_GET['fg'] ) ) {
+                // echo 'FG: ' . $_GET['fg'] . '<br>';
+                // echo 'BG: ' . $_GET['bg'] . '<br>';
+                $this->set_css( $_GET['fg'], $_GET['bg'] );
+            } else {
+                $this->set_css();
+            }
         }
 
+        /**
+         * Echoes HEX code without #.
+         * @param string $type Which color to get: 'bg' or 'color'.
+         */
+        public function get_hex( $type ) {
+            // var_dump($type);
+            if ( $type === 'bg' ) {
+                // var_dump( $this->bg);
+                echo str_replace( '#', '', $this->bg );
+            } elseif ( $type === 'color' ) {
+                echo str_replace( '#', '', $this->color );
+            }
+        }
 
+        /**
+         * Echoes font name.
+         */
+        public function get_font_name() {
+            echo $this->font;
+        }
+
+        /**
+         * Echoes quote text.
+         */
         public function get_quote() {
             echo $this->quote->quoteText;
         }
 
+        /**
+         * Echoes author of quote.
+         */
         public function get_author() {
             if( isset( $this->quote->quoteAuthor ) ) {
                 echo $this->quote->quoteAuthor;
@@ -28,37 +74,64 @@
             }
         }
 
+        /**
+         * Echoes link to quote on Forismatic.
+         */
         public function get_link( $type = false ) {
             echo $this->quote->quoteLink;
         }
 
+        /**
+         * Echoes URL to Google font.
+         */
         public function get_google_font() {
             $font_name = urlencode( $this->font );
             $text = urlencode( $this->get_unique_chars() );
             echo 'https://fonts.googleapis.com/css?family=' . $font_name . '&text=' . $text . '&subset=latin';
         }
 
+        /**
+         * Echos CSS with all colors.
+         */
         public function get_css() {
             echo $this->css;
         }
 
-
+        /**
+         * Returns a link with all parameters so you can come back to the same
+         * quote. Can be passed a string which returns a link minus the part
+         * that has been specified.
+         *
+         * E.g: 'colors' will return URL that will generate new colors, but
+         * keep everything else the same.
+         *
+         * @param bool|string $type Part to switch out: 'colors', 'font', or 'quote'.
+         */
         public function get_quote_link( $type = false ) {
+            $base = 'http://' . $_SERVER['SERVER_NAME'] . '/src/';
+            $colors = 'bg=' . $this->bg . '&fg=' . $this->color;
+            $quote = 'quote=' . urlencode( $this->quote->quoteText ) . '&author=' . urlencode( $this->quote->quoteAuthor );
+            $font = 'font=' . urlencode( $this->font );
+
             switch ( $type ) {
+                // New colors.
                 case 'colors':
-                    echo 'http://' . $_SERVER['SERVER_NAME'] . '/src/?&font=' . urlencode( $this->font );
+                    echo $base . '?' . $font . '&' . $quote;
                     break;
 
+                // New font.
                 case 'font':
-                    echo 'http://' . $_SERVER['SERVER_NAME'] . '/src/?bg=' . $this->bg . '&color=' . $this->color;
+                    echo $base . '?' . $colors . '&' . $quote;
                     break;
 
+                // New quote.
                 case 'quote':
-                    echo 'http://' . $_SERVER['SERVER_NAME'] . '/src/?font=' . urlencode( $this->font ) . '&bg=' . $this->bg . '&color=' . $this->color;
+                    echo $base . '?' . $font . '&' . $colors;
                     break;
 
+                // All params.
                 default:
-                    echo 'http://' . $_SERVER['SERVER_NAME'] . '/src/?font=' . urlencode( $this->font ) . '&bg=' . $this->bg . '&color=' . $this->color;
+                    echo $base . '?' . $font . '&' . $colors . '&' . $quote;
                     break;
             }
 
@@ -80,19 +153,24 @@
         /**
          * Gets random quote from forismatic.com API.
          */
-        protected function random_quote() {
-            $quote = '';
-            if ( isset( $_GET['key'] ) ) {
-                $quote = $this->fetch_quote( urlencode( $_GET['key'] ) );
+        protected function random_quote( $quoteText = false, $quoteAuthor = false ) {
+            if ( $quoteText && $quoteAuthor ) {
+                $quote = new stdClass();
+                $quote->quoteText = urldecode( $quoteText );
+                $quote->quoteAuthor = urldecode( $quoteAuthor );
+                $this->quote = $quote;
+                return $this->quote;
             } else {
                 // TODO: Sometimes a NULL result is returned, figure out how to stop this.
                 $quote = $this->fetch_quote();
+                $this->quote = $quote;
+                return $this->quote;
             }
-
-            $this->quote = $quote;
-            return $this->quote;
         }
 
+        /**
+         * Connects to forismatic.com and gets a new random quote.
+         */
         protected function fetch_quote() {
             $arrContextOptions = array(
                 "ssl" => array(
@@ -127,7 +205,7 @@
             $fonts = json_decode( $fonts, true );
             $fonts_list = $fonts['items'];
 
-            if ( isset( $_GET['font'] ) ) {
+            if ( $font_name ) {
 
                 foreach ( $fonts_list as $font) {
                     if ( $font['family'] === urldecode( $_GET['font'] ) ) {
@@ -147,53 +225,72 @@
             return $font_name;
         }
 
-        protected function set_css() {
-            $a11y_stats = file_get_contents( 'http://www.randoma11y.com/stats/' );
-            $a11y_stats = json_decode( $a11y_stats, true );
-            $count = $a11y_stats['combos'];
 
-            $a11y_color = file_get_contents('http://randoma11y.com/combos?page=' . rand( 1, $count ) . '&per_page=1' );
-            $a11y_color = json_decode( $a11y_color, true );
+        protected function set_css( $foreground = false, $background = false ) {
+            if ( $foreground && $background ) {
+                // Get specific colors.
+                $this->bg = $background;
+                $this->color = $foreground;
+            } else {
+                // Get random colors.
+                $a11y_stats = file_get_contents( 'http://www.randoma11y.com/stats/' );
+                $a11y_stats = json_decode( $a11y_stats, true );
+                $count = $a11y_stats['combos'];
 
+                $color_index = rand( 1, $count );
 
-            $first = $a11y_color[0];
+                $a11y_color = file_get_contents('http://randoma11y.com/combos?page=' . $color_index . '&per_page=1' );
+                $a11y_color = json_decode( $a11y_color, true );
 
-            $this->bg = $first['color_one'];
-            $this->color = $first['color_two'];
+                // var_dump( $a11y_color );
+
+                $first = $a11y_color[0];
+
+                $this->color_id = $first['id'];
+                // var_dump($this->color_id);
+
+                $this->bg = str_replace( '#', '', $first['color_one'] );
+                $this->color = str_replace( '#', '', $first['color_two'] );
+            }
 
             $css = '
             <style>
                 body {
-                    background-color: ' . $this->bg . ';
-                    color: ' . $this->color . '
+                    background-color: #' . $this->bg . ';
+                    color: #' . $this->color . '
                 }
                 .quote-text {
                     font-family: "' . $this->font . '";
                 }
 
                 .quote-link {
-                    color: ' . $this->bg . ';
-                    background-color: ' . $this->color . ';
+                    color: #' . $this->bg . ';
+                    background-color: #' . $this->color . ';
                 }
 
                 .quote-link:hover {
-                    color: ' . $this->color . ';
-                    background-color: ' . $this->bg . ';
+                    color: #' . $this->color . ';
+                    background-color: #' . $this->bg . ';
                 }
 
                 footer.footer-main {
                     background-color: rgba(0, 0, 0, 0.18);
-                    color: ' . $this->color . ';
+                    color: #' . $this->color . ';
                 }
 
                 ::selection {
-                    background: ' . $this->color . ';
+                    background: #' . $this->color . ';
                     text-shadow: none;
-                    color: ' . $this->bg . ';
+                    color: #' . $this->bg . ';
+                }
+
+                footer.footer-main {
+                    background: #' . $this->color . ';
+                    color: #' . $this->bg . ';
                 }
             </style>';
 
             $this->css = $css;
         }
-        
+
     }
--- src/index.php
+++ src/index.php
@@ -3,7 +3,7 @@
     $site = new WebSite();
 ?>
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
@@ -11,11 +11,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="apple-touch-icon" href="apple-touch-icon.png">
-        <!-- Place favicon.ico in the root directory -->
-
         <link rel="stylesheet" href="css/main.css">
-
         <link rel="stylesheet" type="text/css" href="<?php $site->get_google_font(); ?>">
 
         <?php $site->get_css(); ?>
@@ -28,33 +24,47 @@
 
         <main class="main-content">
             <blockquote class="quote">
-                <p class="quote-text">
-                    <?php $site->get_quote(); ?>
-                </p>
+                <p class="quote-text"><?php $site->get_quote(); ?></p>
               <footer class="quote-attribution">
-                <div class="vote">
-                    <button id="vote-up" type="button" name="button">Vote Up</button>
-                    <button id="vote-down" type="button" name="button">Vote Down</button>
-                </div>
-                  —
                   <a class="quote-link" href="<?php $site->get_link(); ?>">
                       <span class="quote-author"><?php $site->get_author(); ?></span>
                   </a>
               </footer>
-              <?php // $site->get_quote_link(); ?>
+              <a href="<?php $site->get_quote_link(); ?>">link</a> |
+              <a href="<?php $site->get_link(); ?>">source</a>
             </blockquote>
 
-            <div class="new-quote">
-                <ul class="quote-actions">
-                    <li><a href="<?php $site->get_quote_link('colors') ?>">New Colors</a></li>
-                    <li><a href="<?php $site->get_quote_link('font') ?>">New Font</a></li>
-                    <li><a href="<?php $site->get_quote_link('quote') ?>">New Quote</a></li>
-                </ul>
+            <div class="actions">
+                <div class="new-quote">
+                    <a class="button" href="<?php $site->get_quote_link('font'); ?>">New Font</a>
+                    <a class="button" href="<?php $site->get_quote_link('colors'); ?>">New Colors</a>
+                    <a class="button" href="<?php $site->get_quote_link('quote'); ?>">New Quote</a>
+                </div>
+                <br>
+                <!-- Like this color combo? Hate this color combo? -->
+                <a class="button" href="http://randoma11y.com/#/?hex=<?php $site->get_hex('bg'); ?>&compare=<?php $site->get_hex('color'); ?>">
+                    colors
+                </a>
+
+                <a class="button" href="https://fonts.google.com/specimen/<?php urlencode( $site->get_font_name() ); ?>">
+                    font
+                </a>
+
+                <!-- <br><br> -->
+                <!-- bg: <span class="color-background"><?php $site->get_hex('bg'); ?></span><br> -->
+                <!-- fg: <span class="color-foreground"><?php $site->get_hex('color'); ?></span><br> -->
+                <!-- font: <span class="font"><?php $site->get_font_name(); ?></span><br> -->
+                <!-- <br> -->
+
             </div>
         </main>
 
         <footer class="footer-main">
-          <div class="colophon">attributions, etc.</p>
+          <p class="colophon">
+              Colors: <a href="http://www.randoma11y.com/">randoma11y.com</a> |
+              Quotes: <a href="http://forismatic.com/">forismatic.com</a><br>
+              Made for <a href="https://a-k-apart.com/">10k Apart</a>
+          </p>
         </footer>
     </body>
 </html>
