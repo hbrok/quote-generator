ce0605b286de3b5a4e04ee2d03214b8dac3d0cc0
Heather B <heatherbrokmeier@gmail.com>
2016-09-07 22:24:18 -0500

    Convert JS to module pattern

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,4 @@
-
-    // console.log('hi');
-
-    // HELPER FUNCTIONS
-
-    /**
-     * foreach function for querySelectorAll elements.
-     */
-    function forEach(array, callback, scope) {
-      for (var i = 0; i < array.length; i++) {
-        callback.call(scope, i, array[i]); // passes back stuff we need
-      }
-    };
-
-    function contains(arr, v) {
-        for(var i = 0; i < arr.length; i++) {
-            if(arr[i] === v) return true;
-        }
-        return false;
-    };
-
-    function unique(str) {
-        var arr = [];
-        for(var i = 0; i < str.length; i++) {
-            if(!contains(arr, str[i])) {
-                arr.push(str[i]);
-            }
-        }
-        return arr;
-    }
-
-
-    /**
-     * Returns a random integer between min (included) and max (included)
-     * Using Math.round() will give you a non-uniform distribution!
-     * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Examples
-     */
-    function getRandomInt(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min)) + min;
-    }
-
-
-    ///////////////////
-
-    // THE REAL STUFF
-
-    // Set default font.
-    var font = document.documentElement.style.getPropertyValue('--font');
+var App = (function () {
 
     // XMLHttpRequest type.
     var invocation = new XMLHttpRequest();
@@ -57,86 +8,122 @@
     var colorCache = [];
     var fontCache = [];
 
+    // Font.
+    var bodyStyles = window.getComputedStyle(document.body);
+    var font = bodyStyles.getPropertyValue('--font');
+
     // API URLs/info.
-    var randoma11yUrl = 'http://www.randoma11y.com/stats/';
-    var forismaticUrl = 'https://crossorigin.me/http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
-
-    var fontsUrl = '/src/font-list.json';
-
     var combos = 113592;
     var colorsURL = 'http://www.randoma11y.com/stats/';
     var colorsPageURL = 'http://randoma11y.com/combos?';
+    var randoma11yUrl = 'http://www.randoma11y.com/stats/';
+    var forismaticUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=jsonp&jsonp=App.getNewQuote';
+    var fontsUrl = '/src/font-list.json';
 
     // Buttons.
     var quoteText = document.getElementById('js-quote-text');
     var quoteAuthor = document.getElementById('js-quote-author');
     var quoteLink = document.getElementById('js-quote-link');
-    var buttons = document.querySelectorAll('.button');
 
-    // Prevent link clicking for all buttons.
-    // forEach(buttons, function (index, value) {
-    //   console.log(index, value); // passes index + value back!
-    //   buttons[index].addEventListener('click', function(e){ //say this is an anchor
-               //do something
-            //   e.preventDefault();
-        //  });
-    // });
-
-    // Set functions for 'Get New Font' button.
     var newFont = document.getElementById('js-new-font');
-    // console.log(newFont);
-    newFont.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        // Send new request if cache is empty, or load from cache.
-        if (fontCache.length === 0) {
-            console.log('1');
-            requestType = 'font';
-            callOtherDomain( fontsUrl );
-        } else if (fontCache.length > 0) {
-            console.log('2');
-            getNewFont();
-        }
-    });
-
-
-    // 'Get New Quote' button.
     var newQuote = document.getElementById('js-new-quote');
-    // console.log(newQuote);
-    newQuote.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        requestType = 'quote';
-        callOtherDomain( forismaticUrl );
-    });
-
-    // 'Get New Colors' button.
     var newColors = document.getElementById('js-new-colors');
-    // console.log(newQuote);
-    newColors.addEventListener('click', function(e) {
-        e.preventDefault();
 
-        // Send new request if cache is empty, or load from cache.
-        if (colorCache.length === 0) {
-            requestType = 'colors';
-            callOtherDomain( colorsPageURL );
-        } else if (colorCache.length > 0) {
-            getNewColors();
+    var init = function () {
+        newFont.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            // Send new request if cache is empty, or load from cache.
+            if (fontCache.length === 0) {
+                console.log('1');
+                requestType = 'font';
+                _callOtherDomain(fontsUrl);
+            } else if (fontCache.length > 0) {
+                console.log('2');
+                getNewFont();
+            }
+        });
+
+        newQuote.addEventListener('click', function (e) {
+            e.preventDefault();
+            //_loadFont(font);
+            _loadScript(forismaticUrl);
+        });
+
+        newColors.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            // Send new request if cache is empty, or load from cache.
+            if (colorCache.length === 0) {
+                requestType = 'colors';
+                _callOtherDomain(colorsPageURL);
+            } else if (colorCache.length > 0) {
+                getNewColors();
+            }
+        });
+    };
+
+    /**
+     * foreach function for querySelectorAll elements.
+     */
+    var _forEach = function (array, callback, scope) {
+        for (var i = 0; i < array.length; i++) {
+            callback.call(scope, i, array[i]); // passes back stuff we need
         }
-    });
+    };
+
+    /**
+     * Check if array contains a value.
+     */
+    var _contains = function (arr, v) {
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i] === v) return true;
+        }
+        return false;
+    };
+
+    /**
+     * Get all unique characters in a string.
+     */
+    var _unique = function (str) {
+        var arr = [];
+        for (var i = 0; i < str.length; i++) {
+            if (!_contains(arr, str[i])) {
+                arr.push(str[i]);
+            }
+        }
+        return arr;
+    };
+
+    /**
+     * Returns a random integer between min (included) and max (included)
+     * Using Math.round() will give you a non-uniform distribution!
+     * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#Examples
+     */
+    var _getRandomInt = function (min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    var _loadScript = function (url) {
+        var wf = document.createElement('script'),
+            s = document.scripts[0];
+        wf.src = url;
+        s.parentNode.insertBefore(wf, s);
+    };
 
 
-
-
-    function callOtherDomain( url ) {
+    var _callOtherDomain = function (url) {
         if (invocation) {
             invocation.open('GET', url, true);
-            invocation.onreadystatechange = handler;
+            invocation.onreadystatechange = _handler;
             invocation.send();
         }
-    }
+    };
 
-    function handler( evtXHR ) {
+
+    var _handler = function (evtXHR) {
         if (invocation.readyState === XMLHttpRequest.DONE) {
             if (invocation.status === 200) {
                 // Get response and replace escaped single quotes with
@@ -147,11 +134,6 @@
 
                 // Different functions based on which button was pressed.
                 switch (requestType) {
-                    case 'quote':
-                        // Get new quote from response.
-                        getNewQuote(response);
-                        break;
-
                     case 'font':
                         // Set font cache, and get new font.
                         fontCache = response.items;
@@ -173,13 +155,33 @@
             }
         }
 
-    }
+    };
+
+    var _getUniqueChars = function () {
+        return _unique(quoteText.innerHTML).join('');
+    };
+
+
+    // TODO: Coda Caption, fonts with only bold styles aren giving an error when not being called with the correct font weight, so we need to also get the (first?) font weight available so they show up. OR only use the font if it has the normal 300/400 default weight....
+// Load a Google font by name.
+    var _loadFont = function (font) {
+        console.log(font);
+        WebFontConfig = {
+            google: {
+                families: [font],
+                text: _getUniqueChars()
+            },
+            timeout: 2000 // Set the timeout to two seconds
+        };
+        _loadScript('https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js');
+    };
 
 
     function getNewQuote(response) {
         // Set new quote, and characters needed for it.
         quoteText.innerHTML = response.quoteText;
-        loadFont(font);
+        _loadFont(font);
+        console.log(font);
 
         // Don't display blank author.
         if (response.quoteAuthor.length > 0)
@@ -192,7 +194,7 @@
     function getNewColors() {
         // Get random color pairing.
         var numColorPairs = colorCache.length;
-        var pair = colorCache[getRandomInt(0, numColorPairs)];
+        var pair = colorCache[_getRandomInt(0, numColorPairs)];
         var colorOne = pair.color_one;
         var colorTwo = pair.color_two;
 
@@ -207,30 +209,13 @@
 
 
     function getNewFont() {
-        font = fontCache[getRandomInt(0, fontCache.length)].family;
-        loadFont(font);
+        font = fontCache[_getRandomInt(0, fontCache.length)].family;
+        _loadFont(font);
         document.documentElement.style.setProperty('--font', font);
     }
 
-
-    function getUniqueChars() {
-        return unique(quoteText.innerHTML).join('');
+    return {
+        init: init,
+        getNewQuote: getNewQuote
     }
-
-
-    // Load a Google font by name.
-    function loadFont(font) {
-      WebFontConfig = {
-          google: {
-            families: [font],
-            text: getUniqueChars()
-          },
-          timeout: 2000 // Set the timeout to two seconds
-        };
-
-      (function(d) {
-          var wf = d.createElement('script'), s = d.scripts[0];
-          wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1.6.16/webfont.js';
-          s.parentNode.insertBefore(wf, s);
-       })(document);
-    };
+}());
