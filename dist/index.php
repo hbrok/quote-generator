bbc9af334f861a5d1cf01baa83cdf89004a381ab
Heather B <heatherbrokmeier@gmail.com>
2017-12-07 21:23:06 -0600

    Update

--- dist/index.php
+++ dist/index.php
@@ -1,6 +1,6 @@
 <?php
     require(__DIR__ . '/../vendor/autoload.php');
-    require_once(__DIR__ . './class-website.php');
+    require_once(__DIR__ . '/class-website.php');
 
     $quotes = new WebSite;
 
